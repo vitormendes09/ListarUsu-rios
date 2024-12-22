68d194d36367d2491e1dd0b941d4b821653c2c15
vitormendes09 <mendes.v@gsuite.iff.edu.br>
2024-12-22 14:38:13 -0300

    Comunicação com banco de dados mongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import fastify from "fastify";
 import cors from '@fastify/cors'
+import { routes } from "./routes";
 
 const app = fastify({ logger: true});
 
 
 const start = async () => {
+
+    await app.register(cors);
+
+    await app.register(routes)
+
     
     try {
         await app.listen({port : 3333})
