d19d75d6b187f95c6edacb0178b4984ee0df2469
vitormendes09 <mendes.v@gsuite.iff.edu.br>
2024-12-24 17:27:06 -0300

    Atualizações do frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 export default function App(){
   return(
     
-    <div>
+    <div className="w-full min-h-screen bg-yellow-950 flex justify-center px-4">
+      <main className="my-10 w-full bg-red-500 md:max-w-2xl">
+        <h1 className="text-4xl font-medium text-white"> Clientes</h1>
+      </main>
       
-      <h1>App</h1>
+     
     </div>
 
   )
